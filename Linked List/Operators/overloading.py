eb06a781d1b578a839e1f966f2f900ae853dc7c9
karthik <karthikpai282@gmail.com>
2026-06-03 21:44:48 +0530

    practice args and **kwargs

--- Linked List/Operators/overloading.py
+++ Linked List/Operators/overloading.py
@@ -12,3 +12,4 @@ mult(2, 4, 3)
 #In the above code, there are two mult() methods, but the python compiler can only see the last i.e the one with 3 parameters. 
 # #Therefore, even though we can define multiple methods with the same name and different arguments, but only the last method of
 #  them can be used. Calling any of the other methods will produce an error. Like here calling will mult(2,3) an error.
+
---
+++ Linked List/Operators/overloading2.py
@@ -0,0 +1,14 @@
+
+@dispatch(int,int)
+def mult(a, b):
+    print(a * b)
+    
+@dispatch(int,int,int)    
+def mult(a, b, c):
+    print(a * b * c)
+    
+    
+mult(2, 3, 5)
+mult(2, 3)
+
+#pip3 install multipledispatch+

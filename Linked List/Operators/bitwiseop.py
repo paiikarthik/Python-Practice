a3e1f10e4365899ce0b60deba13fa11407109c7f
karthik <karthikpai282@gmail.com>
2026-06-03 18:06:26 +0530

    Operators

---
+++ Linked List/Operators/bitwiseop.py
@@ -0,0 +1,34 @@
+aa=6
+print(~aa)
+
+#output -7
+
+bb=40
+print(~bb)
+
+#out put-41
+
+a1=10
+b1=20
+print(a1&b1)
+ 
+a = 20
+b = 8
+ 
+# Print bitwise AND operation 
+print(a & b) 
+ 
+# Print bitwise OR operation 
+print(a | b) 
+ 
+# Print bitwise NOT operation 
+print(~a) 
+ 
+# print bitwise XOR operation 
+print(a ^ b) 
+ 
+# print bitwise right shift operation 
+print(a >> 2) 
+ 
+# print bitwise left shift operation 
+print(a << 2) 
